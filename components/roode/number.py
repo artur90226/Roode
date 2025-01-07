66d564156bd050ebdfc5b22b03c3ebb356cae32a
artur90226 <102323097+artur90226@users.noreply.github.com>
2025-01-07 22:27:46 +0100

    Update number.py
    Add CONF_MIN_VALUE

--- components/roode/number.py
+++ components/roode/number.py
@@ -2,7 +2,7 @@ from typing import OrderedDict
 
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ICON, CONF_MAX_VALUE
+from esphome.const import CONF_ICON, CONF_MAX_VALUE, CONF_MIN_VALUE
 from esphome.cpp_generator import MockObj
 
 from ..persisted_number import PERSISTED_NUMBER_SCHEMA, new_persisted_number
@@ -20,6 +20,7 @@ CONFIG_SCHEMA = cv.Schema(
             {
                 cv.Optional(CONF_ICON, default="mdi:counter"): cv.icon,  # new default
                 cv.Optional(CONF_MAX_VALUE, 10): cv.int_range(-128, 128),
+                cv.Optional(CONF_MIN_VALUE, -10): cv.int_range(-128, 128),
             }
         ),
     }
@@ -28,7 +29,7 @@ CONFIG_SCHEMA = cv.Schema(
 
 async def setup_people_counter(config: OrderedDict, hub: MockObj):
     counter = await new_persisted_number(
-        config, min_value=0, step=1, max_value=config[CONF_MAX_VALUE]
+        config, min_value=config[CONF_MIN_VALUE], step=1, max_value=config[CONF_MAX_VALUE]
     )
     cg.add(hub.set_people_counter(counter))
 
