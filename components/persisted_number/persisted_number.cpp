3863f91e4ad1c0e91a6b2337a46f890a6b82a79e
artur90226 <102323097+artur90226@users.noreply.github.com>
2025-01-07 22:24:35 +0100

    Update persisted_number.cpp
    Set Number to 0

--- components/persisted_number/persisted_number.cpp
+++ components/persisted_number/persisted_number.cpp
@@ -14,14 +14,16 @@ auto PersistedNumber::control(float newValue) -> void {
 auto PersistedNumber::setup() -> void {
   float value;
   if (!this->restore_value_) {
-    value = this->traits.get_min_value();
+    //value = this->traits.get_min_value();
+    value = 0;
   } else {
     this->pref_ = global_preferences->make_preference<float>(this->get_object_id_hash());
     if (this->pref_.load(&value)) {
       ESP_LOGI("number", "'%s': Restored state %f", this->get_name().c_str(), value);
     } else {
       ESP_LOGI("number", "'%s': No previous state found", this->get_name().c_str());
-      value = this->traits.get_min_value();
+      //value = this->traits.get_min_value();
+      value = 0;
     }
   }
   this->publish_state(value);
